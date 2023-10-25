fc3962156d60398c6db73a6145802065f4d98d54
sbx_user1051 <sbx_user1051@169.254.92.209>
2023-10-25 16:15:58 +0000

    Wed Oct 25 16:15:58 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,4 +5,21 @@
 def checkout(skus):
     price_table = {
         'A': {'price': 50, 'special_offer': {'count': 3, 'price': 130}},
+        'B': {'price': 30, 'special_offer': {'count': 2, 'price': 45}},
+        'C': {'price': 20, 'special_offer': None},
+        'D': {'price': 15, 'special_offer': None},
     }
+
+    item_count = {
+        'A': 0,
+        'B': 0,
+        'C': 0,
+        'D': 0,
+    }
+
+    total_price = 0
+
+    for item in skus:
+        if item in item_count:
+            item_count[item] += 1
+
